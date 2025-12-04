0bdeff16f96dabbbbc4da11990e78aa40b34cd22
TheAFoxe <150961694+TheAFoxe@users.noreply.github.com>
2025-12-04 13:36:10 +0100

    School

--- source/objects/audio_caster/audio_cast.gd
+++ source/objects/audio_caster/audio_cast.gd
@@ -3,64 +3,75 @@ extends Node3D
 class_name AudioCast
 
 @export var ray_length := 1000.0
+@export var max_bounces := 10
+@export_flags_3d_physics var collision_mask: int
 
-var frame_count
 
-var _debug_line: MeshInstance3D
+var debug_line: ImmediateMesh
+var instance: MeshInstance3D
 
 @onready var root := $".."
+@onready var sphere := $"../DebugSphere"
 
 
 func _ready() -> void:
-	pass
+	var mat = StandardMaterial3D.new()
+	mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	mat.albedo_color = Color.RED
+	
+	debug_line = ImmediateMesh.new()
+	instance = MeshInstance3D.new()
+	instance.material_override = mat
+	instance.mesh = debug_line
+
+	add_child(instance)
 
 
 func _physics_process(delta: float) -> void:
+	debug_line.clear_surfaces()
 	cast()
 
 
 func cast() -> void:
-	var debug_array: PackedVector3Array
+	debug_line.surface_begin(Mesh.PRIMITIVE_LINES)
 	var space_state := get_world_3d().direct_space_state
-	var origin := self.global_position
-	var end := (self.global_position + ray_length * Vector3.FORWARD).rotated(Vector3.UP, root.rotation.y)
-	var query := PhysicsRayQueryParameters3D.create(origin, end)
-	query.collide_with_bodies = true
-	query.collision_mask = 0x00001001
+	var query := PhysicsRayQueryParameters3D.create(Vector3.ZERO, Vector3.ZERO, collision_mask)
+	var direction := Vector3.FORWARD.rotated(Vector3.UP, root.rotation.y)
+	query.from = global_position
+	query.to = query.from + direction * ray_length
+	query.collide_with_areas = true
 	var result := space_state.intersect_ray(query)
 	
-	while true:
-		debug_array.append(query.from)
-		if result:
-			debug_array.append(result.position)
-		else:
-			debug_array.append(query.to)
+	for i in max_bounces:
+		if not result: draw_debug(query.from, query.to); break
+		if i == max_bounces: draw_debug(query.from, result.position); break
+		draw_debug(query.from, result.position)
 		
-		if not result: break
+		if result.collider is Area3D:
+			sound(query.from, query.to, result.collider.global_position, direction)
+			query.from = result.position + direction * 0.1
+			result = space_state.intersect_ray(query)
+			if not result: draw_debug(query.from, query.to); break
+			draw_debug(query.from, result.position)
+		
+		
+		
 		query.from = result.position
-		query.to = (result.position - origin).normalized().bounce(result.normal) * ray_length + result.position
+		direction = direction.bounce(result.normal)
+		query.to = query.from + direction * ray_length
 		result = space_state.intersect_ray(query)
 	
-	draw_debug(debug_array)
+	debug_line.surface_end()
 
 
-func draw_debug(debug_array) -> void:
-	var debug_line: ImmediateMesh
-	
-	if _debug_line:
-		_debug_line.queue_free()
-	
-	_debug_line = MeshInstance3D.new()
-	debug_line = ImmediateMesh.new()
-	
-	for i in debug_array.size() - 1:
-		var material := StandardMaterial3D.new()
-		material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-		material.albedo_color = Color.DARK_RED
-		debug_line.surface_begin(Mesh.PRIMITIVE_LINES, material)
-		debug_line.surface_add_vertex(to_local(debug_array.get(i)))
-		debug_line.surface_add_vertex(to_local(debug_array.get(i + 1)))
-		debug_line.surface_end()
-	
-	_debug_line.mesh = debug_line
-	add_child(_debug_line)
+func draw_debug(from, to) -> void:
+	debug_line.surface_add_vertex(to_local(from))
+	debug_line.surface_add_vertex(to_local(to))
+
+
+func sound(line_start : Vector3, line_end : Vector3, point_position : Vector3, direction : Vector3):
+	var line_direction := (line_start - line_end).normalized()
+	var vector_to_object := point_position - line_start
+	var distance := line_direction.dot(vector_to_object)
+	var closest_position := line_start + distance * line_direction
+	sphere.global_position = closest_position + direction.rotated(Vector3.UP, PI) * 2
