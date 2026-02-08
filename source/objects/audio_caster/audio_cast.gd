8789b819bf80ba9df89a7ed2438d7b16c569c5f7
TheAFoxe <schuninivan@gmail.com>
2026-02-08 22:04:53 +0100

    fal

--- source/objects/audio_caster/audio_cast.gd
+++ source/objects/audio_caster/audio_cast.gd
@@ -1,8 +1,12 @@
 extends PickableObject
 class_name AudioCast
 
-signal send_chord_to_hitted(receiver, chord)
+# Constants
+const RAY_OFFSET_FROM_SURFACE: float = 0.005
+const INACTIVE_AUDIO_HEIGHT: int = 50
+const AUDIO_PLAYER_OFFSET: float = 1.0
 
+# Exported values
 @export var debug: bool
 
 @export_category("Chord")
@@ -19,165 +23,221 @@ signal send_chord_to_hitted(receiver, chord)
 @export var max_bounces: int
 @export_flags_3d_physics var collision_mask: int
 
-var debug_line: ImmediateMesh
-var instance: MeshInstance3D
-var _active_audio_players: Array[int]
-var audio_debug_array := []
-var audio_debug: MeshInstance3D
-var audio_streamer_array := []
+# Public variables
 var current_hit: Node3D
-var last_hit: Node3D
-var polyphonic_stream: AudioStreamPolyphonic
 var chord: Array
 
-@onready var query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(Vector3.ZERO, Vector3.ZERO, collision_mask)
-@onready var root := $".."
-@onready var sound_timer: Timer = $Timer
-@onready var audio_caster: Marker3D = $AudioCast
+# Private variables
+var _debug_line_mesh: ImmediateMesh
+var _debug_mesh_instance: MeshInstance3D
+var _active_audio_player_indices: Array[int] = []
+var _audio_debug_spheres: Array[MeshInstance3D] = []
+var _audio_streamers: Array[AudioStreamPlayer3D] = []
+var audio_debug: MeshInstance3D
+var last_hit: Node3D
+var _polyphonic_stream: AudioStreamPolyphonic
+
+var _current_ray_hit_path: Array[Node3D] = []
+var _last_ray_hit_path: Array[Node3D] = []
+
+# Onready variables
+@onready var _ray_query: PhysicsRayQueryParameters3D = (
+	PhysicsRayQueryParameters3D.create(
+		Vector3.ZERO,
+		Vector3.ZERO,
+		collision_mask
+	)
+)
+@onready var _sound_timer: Timer = $Timer
+@onready var _audio_caster: Marker3D = $AudioCast
 
 
 func _ready() -> void:
 	super()
+	
 	can_move = false
 	global_position.y = 1
 	player_camera_marker = $PlayerCameraMarker
 	
-	max_bounces = max_bounces + 1
+	_current_ray_hit_path.resize(max_bounces)
+	_last_ray_hit_path.resize(max_bounces)
+	_current_ray_hit_path.fill(null)
+	_last_ray_hit_path.fill(null)
+	
+	chord = [
+		chord_resource.s6,
+		chord_resource.s5,
+		chord_resource.s4,
+		chord_resource.s3,
+		chord_resource.s2,
+		chord_resource.s1
+	]
+	
+	_sound_timer.one_shot = false
+	_sound_timer.start()
+	
+	for i in max_bounces:
+		_current_ray_hit_path.append(null)
+		_last_ray_hit_path.append(null)
 	
 	chord = [chord_resource.s6, chord_resource.s5, chord_resource.s4, chord_resource.s3, chord_resource.s2, chord_resource.s1]
 	
-	sound_timer.one_shot = false
-	sound_timer.start()
-	
-	for i in max_bounces:
-		audio_debug = MeshInstance3D.new()
-		audio_debug.mesh = SphereMesh.new()
-		audio_debug.mesh.radius = 0.1
-		audio_debug.mesh.height = 0.2
-		add_child(audio_debug)
-		audio_debug.position.y = i + 0.2
-		audio_debug_array.append(audio_debug)
-	
-	# AUDIO
-	for i in max_bounces:
-		var audio_stream = AudioPlayer.new()
-		audio_stream.stream = polyphonic_stream
-		audio_stream.chord = chord_resource
-		audio_stream.max_distance = audio_max_distance
-		audio_stream.max_db = audio_max_volume
-		audio_stream.volume_db = audio_volume
-		add_child(audio_stream)
-		audio_streamer_array.append(audio_stream)
-	
-	# MATERIAL
-	var mat = StandardMaterial3D.new()
-	mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	mat.albedo_color = Color.RED
-	debug_line = ImmediateMesh.new()
-	instance = MeshInstance3D.new()
-	instance.material_override = mat
-	instance.mesh = debug_line
-	add_child(instance)
+	_create_debug_visualization()
+	_create_audio_players()
 
 
 func _physics_process(delta: float) -> void:
-	debug_line.clear_surfaces()
-	_active_audio_players.clear()
+	_debug_line_mesh.clear_surfaces()
+	_active_audio_player_indices.clear()
 	
-	for i in audio_debug_array:
+	for i in _audio_debug_spheres:
 		i.hide()
 	
 	cast()
 	
-	for i in audio_streamer_array.size():
-		if i in _active_audio_players: continue
-		audio_streamer_array.get(i).global_position = Vector3(0, 50, 0)
+	for i in _audio_streamers.size():
+		if i not in _active_audio_player_indices:
+			_audio_streamers.get(i).global_position = Vector3(0, INACTIVE_AUDIO_HEIGHT, 0)
 
 
 func cast() -> void:
-	var player_hit_count: int = 0
-	var player_hitted: bool = false
-	var player_position: Vector3
+	var player_hitted: bool = false 
 	var current_bounce: int = 0
 	var result: Dictionary
 	var space_state := get_world_3d().direct_space_state
 	var direction := -global_transform.basis.z
 	var current_start := global_position
 	
-	query.exclude = []
-	query.collide_with_areas = true
-	query.hit_from_inside = true
+	_current_ray_hit_path.fill(null)
+	
+	_ray_query.exclude = []
+	_ray_query.collide_with_areas = true
+	_ray_query.hit_from_inside = true
 	
 	if debug:
-		debug_line.surface_begin(Mesh.PRIMITIVE_LINES)
+		_debug_line_mesh.surface_begin(Mesh.PRIMITIVE_LINES)
 	
 	while current_bounce < max_bounces:
-		query.from = current_start
-		query.to = query.from + direction * ray_length
-		result = space_state.intersect_ray(query)
+		var previous_hit_at_index: Node3D = _last_ray_hit_path[current_bounce]
+		
+		_ray_query.from = current_start
+		_ray_query.to = _ray_query.from + direction * ray_length
+		result = space_state.intersect_ray(_ray_query)
+		
+		if player_hitted:
+			_place_audio_at_player(
+				_ray_query.from,
+				result.position if result else _ray_query.to,
+				current_hit.position,
+				current_bounce,
+				direction
+				)
+			player_hitted = false
 		
 		if not result:
-			if player_hitted:
-				sound(query.from, query.to, player_position, player_hit_count, direction)
-			draw_debug(query.from, query.to)
+			_draw_debug_line(_ray_query.from, _ray_query.to)
 			current_hit = null
+			_current_ray_hit_path[current_bounce] = current_hit
+
+			if previous_hit_at_index is AudioCatcher:
+				previous_hit_at_index.deactivate()
 			break
 		
 		var collider = result.collider
 		current_hit = collider.owner if collider.owner else collider
-		if current_hit == null:
-			push_error("Current hit is null, returning")
-			return
 		
 		if current_hit is Player:
+			_ray_query.exclude = [result.rid]
 			player_hitted = true
-			player_position = result.collider.global_position
-			player_hit_count += 1
-			query.exclude = [result.rid]
 			continue
-		if player_hitted:
-			sound(query.from, result.position, player_position, player_hit_count, direction)
-			player_hit_count += 1
-			player_hitted = false
 		
-		if current_hit != last_hit:
-			if current_hit is AudioCatcher:
-				current_hit.activate()
-			if last_hit is AudioCatcher:
-				last_hit.deactivate()
-			if current_hit.has_method("receive_chord"):
-				current_hit.receive_chord(chord)
-			last_hit = current_hit
+		_current_ray_hit_path[current_bounce] = current_hit
+		
+		if current_hit != previous_hit_at_index:
+			_update_audio_catcher_state(current_hit, previous_hit_at_index)
 		
 		if current_hit is AudioCatcher:
-			draw_debug(query.from, result.position)
+			_draw_debug_line(_ray_query.from, result.position)
 			break
 		
-		
-		draw_debug(query.from, result.position)
-		query.exclude = []
+		_draw_debug_line(_ray_query.from, result.position)
+		_ray_query.exclude = []
 		direction = direction.bounce(result.normal)
-		current_start = result.position + (result.normal * 0.005)
+		current_start = result.position + (result.normal * RAY_OFFSET_FROM_SURFACE)
 		current_bounce += 1
 	
-	if debug: debug_line.surface_end()
-
-
-func draw_debug(from, to) -> void:
-	if not debug: return
-	debug_line.surface_add_vertex(to_local(from))
-	debug_line.surface_add_vertex(to_local(to))
-
-
-func sound(line_start: Vector3, line_end: Vector3, point_position: Vector3, id: int, direction: Vector3):
-	var audio_stream: AudioStreamPlayer3D = audio_streamer_array.get(id)
+	_last_ray_hit_path = _current_ray_hit_path.duplicate()
 	
-	var closest_position := Geometry3D.get_closest_point_to_segment(point_position - direction, line_start, line_end)
-	audio_stream.global_position = closest_position
+	if debug: _debug_line_mesh.surface_end()
+
+
+func _create_debug_visualization() -> void:
+	for i in max_bounces:
+		var sphere := MeshInstance3D.new()
+		sphere.mesh = SphereMesh.new()
+		sphere.mesh.radius = 0.1
+		sphere.mesh.height = 0.2
+		add_child(sphere)
+		_audio_debug_spheres.append(sphere)
+		
+	var mat = StandardMaterial3D.new()
+	mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	mat.albedo_color = Color.RED
 	
-	_active_audio_players.append(id)
+	_debug_line_mesh = ImmediateMesh.new()
+	_debug_mesh_instance = MeshInstance3D.new()
+	_debug_mesh_instance.material_override = mat
+	_debug_mesh_instance.mesh = _debug_line_mesh
+	add_child(_debug_mesh_instance)
+
+
+func _create_audio_players() -> void:
+	for i in max_bounces:
+		var audio_stream = AudioPlayer.new()
+		audio_stream.stream = _polyphonic_stream
+		audio_stream.chord = chord_resource
+		audio_stream.max_distance = audio_max_distance
+		audio_stream.max_db = audio_max_volume
+		audio_stream.volume_db = audio_volume
+		add_child(audio_stream)
+		_audio_streamers.append(audio_stream)
+
+
+func _update_audio_catcher_state(new_hit: Node3D, previous_hit: Node3D) -> void:
+		if new_hit is AudioCatcher:
+			new_hit.activate()
+		if previous_hit is AudioCatcher:
+			previous_hit.deactivate()
+
+
+func _place_audio_at_player(line_start: Vector3, line_end: Vector3, player_position: Vector3, audio_id: int, ray_direction: Vector3) -> void:
+	if audio_id >= _audio_streamers.size():
+		push_warning(
+			"Audio player ID %d exceeds array size %d" % [
+				audio_id,
+				_audio_streamers.size()
+				]
+			)
+		return
 	
+	var audio_player: AudioStreamPlayer3D = _audio_streamers[audio_id]
+
+	var closest_position := Geometry3D.get_closest_point_to_segment(
+		player_position - ray_direction * AUDIO_PLAYER_OFFSET,
+		line_start,
+		line_end
+		)
+	audio_player.global_position = closest_position
+
+	_active_audio_player_indices.append(audio_id)
+
 	if debug:
-		audio_debug = audio_debug_array.get(id)
-		audio_debug.global_position = audio_stream.global_position
-		audio_debug.show()
+		var debug_sphere := _audio_debug_spheres[audio_id]
+		debug_sphere.global_position = audio_player.global_position
+		debug_sphere.show()
+
+
+func _draw_debug_line(from: Vector3, to: Vector3) -> void:
+	if not debug: return
+	_debug_line_mesh.surface_add_vertex(to_local(from))
+	_debug_line_mesh.surface_add_vertex(to_local(to))
